7d2e808aa6e9443d3cf5d03ef6aee2e610bd4554
punit <punit.pradhan@innoraft.com>
2023-10-11 18:52:26 +0530

    made changes according review

--- web/themes/custom/movie_portal/js/popup.js
+++ web/themes/custom/movie_portal/js/popup.js
@@ -1,21 +1,24 @@
 (function ($, Drupal, once) {
   Drupal.behaviors.moviePage = {
     attach: function (context, settings) {
-      let modalDiv = $("#expanded_popup");
-      let closeTag = $("#close_btn");
-      let expandedImage = $("#expandedImage");
-      let movieTitle = $("#movieTitle");
-      let modalInfo = $("#modalInfo");
+      const modalDiv = $("#expanded_popup");
+      const closeTag = $("#close_btn");
+      const expandedImage = $("#expandedImage");
+      const movieTitle = $("#movieTitle");
+      const modalInfo = $("#modalInfo");
+      const parentSelector = "#block-movie-portal-content";
+      const viewSelector = ".view-homepage.view-id-homepage.view-display-id-page_1"; 
 
-      once("popUp", ".view-display-id-page_1", context).forEach((element) => {
-        const views = $(".view-display-id-page_1");
-        
-        let on_click_img = (e) => {
+      once("popUp", parentSelector + " " + viewSelector, context).forEach((element) => {
+        const views = $(parentSelector + " " + viewSelector);
+
+        const on_click_img = (e) => {
           if (e.target.tagName === "IMG") {
-            let element = $(e.target).parent();
-            let title = element.find("h1").text();
-            let image = $(e.target).attr("src");
-            let description = element.find("h3").text();
+            const element = $(e.target).closest(".movie_card");
+            const title = element.find(".movie_title").text();
+            const image = element.find("img").attr("src");
+            const description = element.find(".movie_description").text();
+
             modalDiv.css("display", "block");
             expandedImage.attr("src", image);
             movieTitle.text(title);
@@ -29,7 +32,7 @@
       });
 
       once("popUp", ".expanded", context).forEach((element) => {
-        let handleCloseClick = () => {
+        const handleCloseClick = () => {
           modalDiv.css("display", "none");
         };
         closeTag.on("click", handleCloseClick);
